f4c6541050700f8026c0310ece6d1b0bdb047444
zhangxing.miner <zhangxing.miner@bytedance.com>
2019-05-10 12:42:28 +0800

    chore: travis

---
+++ release.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const { execSync } = require('child_process')
+
+function cmd(command) {
+  try {
+    const result = execSync(command)
+    return result.toString()
+  } catch (error) {
+    throw error
+  }
+}
+
+function getVersion() {
+  try {
+    let tag = cmd('git describe --exact-match --tags $(git rev-parse HEAD)')
+    tag = tag.replace(/\r?\n|\r/g, '')
+    if (/^v\d+.\d+.\d+$/.test(tag)) {
+      return tag.slice(1)
+    }
+
+    return ''
+  } catch (error) {
+    return ''
+  }
+}
+
+const packageInfo = JSON.parse(fs.readFileSync('package.json'))
+const version = getVersion()
+if (version) {
+  packageInfo.version = version
+}
+
+fs.writeFileSync('package.json', JSON.stringify(packageInfo, null, 2))
