bff78d5d34154d61629505af76120ddad89a816b
zhangxing.miner <zhangxing.miner@bytedance.com>
2019-10-21 21:15:53 +0800

    fix: translation

--- utils/translate.js
+++ utils/translate.js
@@ -127,9 +127,9 @@ const translate = async input => {
   }
 }
 
-;(async () => {
-  console.log(await translate('test'))
-})()
+// (async () => {
+//   console.log(await translate("test"));
+// })();
 
 module.exports = {
   translate
